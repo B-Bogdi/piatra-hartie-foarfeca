1cc1137b45776d9187ead92661bf7c5e90128e4b
B-Invisible <114253054+B-Invisible@users.noreply.github.com>
2023-04-09 20:34:11 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,104 @@
-const rollBtn = document.getElementById("roll")
-rollBtn.addEventListener("click", play)
-function play() {
-    let zar1 = Math.floor(Math.random() * 6) + 1
-    let zar2 = Math.floor(Math.random() * 6) + 1
-    switch (zar1) {
-        case 1: dice1.innerHTML = '<img src="images/fataZar1.jpg" />';
-            break;
-        case 2: dice1.innerHTML = '<img src="images/fataZar2.jpg" />';
-            break;
-        case 3: dice1.innerHTML = '<img src="images/fataZar3.jpg" />';
-            break;
-        case 4: dice1.innerHTML = '<img src="images/fataZar4.jpg" />';
-            break;
-        case 5: dice1.innerHTML = '<img src="images/fataZar5.jpg" />';
-            break;
-        case 6: dice1.innerHTML = '<img src="images/fataZar6.jpg" />';
-            break;
+const piatraChoose = document.getElementById("piatra")
+const hartieChoose = document.getElementById("hartie")
+const foarfecaChoose = document.getElementById("foarfeca")
+const start = document.getElementById("start")
+const resetBtn = document.getElementById("reset")
+const introPhoto = document.getElementById("introphoto")
+const introText = document.getElementById("introtext")
+const buttonContainer = document.getElementById("buttons")
+
+myScore = 0
+botScore = 0
+
+let myScoreTable = document.getElementById("eu")
+let botScoreTable = document.getElementById("bot")
+let scoreTable = document.getElementById("score")
+
+botChoose = document.getElementById("botchoose")
+stats = document.getElementById("status")
+mySign = document.getElementById("mychoose")
+
+myScoreTable.innerHTML = "<p>" + `${myScore}` + "</p>"
+botScoreTable.innerHTML = "<p>" + `${botScore}` + "</p>"
+
+resetBtn.addEventListener("click", reset)
+
+function reset() {
+    window.location.reload();
+}
+
+start.addEventListener("click", game)
+let myChoose
+function game() {
+
+    introPhoto.style.display = "none"
+    introText.style.display = "none"
+    start.style.display = "none"
+
+    resetBtn.style.display = "inherit"
+    buttonContainer.style.display = "inherit"
+    scoreTable.style.display = "block"
+    stats.style.display = "block"
+
+    piatraChoose.addEventListener("click", choose1)
+    function choose1() {
+        mySign.innerHTML = '<p class="mysignp"> <img class="mysign" src="piatra.png" alt="" /></p>'
+        myChoose = 1;
+        rndInt = Math.floor(Math.random() * (3 - 1 + 1) + 1)
+        console.log(rndInt)
+        switch (rndInt) {
+            case 1: stats.innerHTML = "<p>Egal</p>"
+
+                botChoose.innerHTML = '<div class="botsignp"> <img class="botsign" src="piatra.png" alt="" /> </div>'
+                break;
+            case 2: stats.innerHTML = "<p>Ai pierdut</p>"
+                botChoose.innerHTML = '<p class="botsignp"> <img class="botsign" src= "hartie.png" alt="" /></p>'
+                botScoreTable.innerHTML = "<p>" + `${botScore = botScore + 1}` + "</p>"
+                break;
+            case 3: stats.innerHTML = "<p>Ai castigat</p>"
+                botChoose.innerHTML = '<p class="botsignp"> <img class="botsign" src="foarfeca.png" alt="" /></p>'
+                myScoreTable.innerHTML = "<p>" + `${myScore = myScore + 1}` + "</p>"
+                break;
+        }
     }
-    switch (zar2) {
-        case 1: dice2.innerHTML = '<img src="images/fataZar1.jpg" />';
-            break;
-        case 2: dice2.innerHTML = '<img src="images/fataZar2.jpg" />';
-            break;
-        case 3: dice2.innerHTML = '<img src="images/fataZar3.jpg" />';
-            break;
-        case 4: dice2.innerHTML = '<img src="images/fataZar4.jpg" />';
-            break;
-        case 5: dice2.innerHTML = '<img src="images/fataZar5.jpg" />';
-            break;
-        case 6: dice2.innerHTML = '<img src="images/fataZar6.jpg" />';
-            break;
+    hartieChoose.addEventListener("click", choose2)
+    function choose2() {
+        mySign.innerHTML = '<p class="mysignp"> <img class="mysign" src="hartie.png" alt="" /></p>'
+        myChoose = 2;
+        rndInt = Math.floor(Math.random() * (3 - 1 + 1) + 1)
+        console.log(rndInt)
+        switch (rndInt) {
+            case 1: botChoose.innerHTML = '<div class="botsignp"> <img class="botsign" src="piatra.png" alt="" /> </div>'
+                stats.innerHTML = "<p>Ai castigat</p>"
+                myScoreTable.innerHTML = "<p>" + `${myScore = myScore + 1}` + "</p>"
+                break;
+            case 2: stats.innerHTML = "<p>Egal</p>"
+                botChoose.innerHTML = '<p class="botsignp"> <img class="botsign" src="hartie.png" alt="" /></p>'
+                break;
+            case 3: stats.innerHTML = "<p>Ai pierdut</p>"
+                botChoose.innerHTML = '<p class="botsignp"> <img class="botsign" src="foarfeca.png" alt="" /></p>'
+                botScoreTable.innerHTML = "<p>" + `${botScore = botScore + 1}` + "</p>"
+                break;
+        }
     }
-    let score = zar1 + zar2
-    valuebox.innerHTML = score
-    list.innerHTML += "<hr><li>" + score + "</li>"
+    foarfecaChoose.addEventListener("click", choose3)
+    function choose3() {
+        mySign.innerHTML = '<p class="mysignp"> <img class="mysign" src="foarfeca.png" alt="" /></p>'
+        myChoose = 3;
+        rndInt = Math.floor(Math.random() * (3 - 1 + 1) + 1)
+        console.log(rndInt)
+        switch (rndInt) {
+            case 1: botChoose.innerHTML = '<div class="botsignp"> <img class="botsign" src="piatra.png" alt="" /> </div>'
+                stats.innerHTML = "<p>Ai pierdut</p>"
+                botScoreTable.innerHTML = "<p>" + `${botScore = botScore + 1}` + "</p>"
+                break;
+            case 2: stats.innerHTML = "<p>Ai castigat</p>"
+                botChoose.innerHTML = '<p class="botsignp"> <img class="botsign" src="hartie.png" alt="" /></p>'
+                myScoreTable.innerHTML = "<p>" + `${myScore = myScore + 1}` + "</p>"
+                break;
+            case 3: stats.innerHTML = "<p>Egal</p>"
+                botChoose.innerHTML = '<p class="botsignp"> <img class="botsign" src="foarfeca.png" alt="" /></p>'
+                break;
+        }
+    }
 }
